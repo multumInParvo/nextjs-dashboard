337ba1819cf817b2cc66fa4854633c0e96c3b8bf
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-08-15 12:25:04 +0200

    Learn about Partial Prerendering, a new rendering model introduced in Next.js

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
+const experimental_ppr = true;
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
